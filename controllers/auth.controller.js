671c8ef2e85a8d1afa38b987d2ec4a95903e66f6
valelectronic <nnanwikevictor@gmail.com>
2025-04-15 21:26:35 +0100

    setting up the user model

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,13 +1,25 @@
 import express from 'express';
 
+
+// for signing up a user
 export const signUp = async(req, res) => {
-    res.send("Sign Up Route")
+
+
+
+res.send("signup Up Route")
+
+
+
     
 }
+
+
 export const logIn = async(req, res) => {
     res.send("login Up Route")
     
 }
+
+
 export const logOut = async(req, res) => {
     res.send("logout Up Route")
     
---
+++ models/user.model.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required"],
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required"],
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required"],
+        minlength: [6, "Password must be at least 6 characters long"],
+    },
+    cartItem:[
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true,
+            },
+            quantity: {
+                type: Number,
+                required: true,
+                default: 1,
+            },
+        },
+    ],
+    role:{
+        type: String,
+        enum: ["customer", "admin"],
+        default: "customer",
+    }
+}, { timestamps: true });
+
+const User = mongoose.model("User", userSchema);
+
+// Middleware to hash password before saving to the database
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+    
+});
+
+// Method to compare password for login
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+
+export default User;
+
